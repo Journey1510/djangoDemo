7cec68859491173c7d026b539909c8c69a51df05
zhangcj01 <zhangcj01@inspur.com>
2019-04-01 17:13:05 +0800

    增加首页

--- djangoDemo/urls.py
+++ djangoDemo/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from django.urls import path
 from django.views.static import serve
 
-from . import view, testdb, search, search2, settings
+from blog.views import IndexView
+from . import view, search, search2, settings
 
 urlpatterns = [url(r'^admin/', admin.site.urls),
+               url(r'^$', IndexView.as_view(), name='index'),
                url(r'^media/(?P<path>.*)$', serve, {"document_root": settings.MEDIA_ROOT}),
                url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
                path('hello/', view.hello),
-               path('testdb/', testdb.testdb),
                path('search', search.search),
                path('search_form', search.search_form),
                path('search_post', search2.search_post)]
